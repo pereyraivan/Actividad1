729d4c3943d913288a3244f5712fa9b32de66d78
Ivan <pereyraivan@hotmail.com>
2021-10-26 00:02:22 -0300

    Agregar archivos de proyecto.

---
+++ Context/PostsDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Actividad1.MODELO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad1.DataContext
+{
+    class PostsDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-CH99N01;Initial Catalog=Actividad1;Integrated Security=True");
+        }
+        public PostsDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+        public DbSet<Posts> Posts { get; set; }
+    }
+}
---
+++ MODELO/Comments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad1.MODELO
+{
+    class Comments
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Comments Comment { get; set; }
+        public ICollection<Usuario> Usuarios{ get; set; }
+    }
+}
